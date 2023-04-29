fb9d5b90accfe4003d98f5e706a3f64c5385568f
Eedal <elkindearmas2015@gmail.com>
2023-04-29 14:12:42 -0500

    [CONFIG] add babel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-require('./database');
+import express from 'express';
+import './database'
+import router from './routes/index.routes';
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
 
-app.use(require('./routes/index.routes'));
+app.use(router);
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
