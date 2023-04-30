236d813f4c19d3b59790d0c6aad316d56f4cb5f1
Eedal <42855161+Eedal@users.noreply.github.com>
2023-04-30 01:14:37 -0500

    Revert "[ADD] add create and findby filter invoice"

--- src/routes/invoice.routes.ts
+++ src/routes/invoice.routes.ts
@@ -7,17 +7,10 @@ import {
   updateInvoice,
   deleteInvoice,
 } from "../controllers/invoice.controller";
-import { authMiddleware } from "../auth/auth.middleware";
 
-const protectedRoutes = (router: express.Router) => {
-  router.use(authMiddleware);
-
-  router.get("/invoices", getInvoices);
-  router.post("/invoices", createInvoice);
-  router.put("/invoices/:id", updateInvoice);
-  router.delete("/invoices/:id", deleteInvoice);
-};
-
-protectedRoutes(router);
+router.get("/invoices", getInvoices);
+router.post("/invoices", createInvoice);
+router.put("/invoices/:id", updateInvoice);
+router.delete("/invoices/:id", deleteInvoice);
 
 export default router;
--- src/utils/invoice.ts
+++
@@ -1,5 +0,0 @@
-import { ulid } from "ulid";
-
-export const generateNit = () => {
-  return ulid();
-};
